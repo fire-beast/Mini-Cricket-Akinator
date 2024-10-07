0711b0ac6f5f9661f7305a5682e047aefaff8fe2
fire-beast <143997267+fire-beast@users.noreply.github.com>
2024-10-07 20:55:15 +0530

    Add files via upload

---
+++ akinator_main.cpp
@@ -0,0 +1,362 @@
+#include<iostream>
+using namespace std;
+class jeet
+{
+private:
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,xx,yy,z,ab,cd,ef,gh,ij,lm=0,ny,zf=0,op,abc,hi,rcb,sr,dk,sj,ris;
+public:
+    char name[30];
+    void game();
+};
+inline void jeet::game()
+{
+system("Color 30");
+cout<<"Hey there! Welcome to Akinator!"<<endl;
+cout<<endl;
+cout<<"this is a game where i will try to guess the current indian cricketer which you might be thinking of!"<<endl;
+cout<<endl;
+cout<<"enter 1 if yes or 2 if no!"<<endl;
+cout<<endl;
+while(lm==0)
+{
+    cout<<"press 5 to Start the Game!"<<endl;
+cin>>a;
+if(a==5){
+cout<<"is your character male?"<<endl;
+cin>>b;
+zf++;
+if(b==1){
+cout<<"is your character aged more than 30?"<<endl;
+cin>>c;
+zf++;
+if(c==1)
+{
+    cout<<"has your character been into commentary?"<<endl;
+    cin>>dk;
+    zf++;
+    if(dk==1){
+        cout<<"your character is dinesh karthik"<<endl;
+    }
+    else if(dk==2){
+    cout<<"is your character from maharashtra?!"<<endl;
+    cin>>d;
+    zf++;
+    if(d==1){
+        cout<<"has your character been a captain of the indian wc team?"<<endl;
+        cin>>e;
+        zf++;
+        if(e==1){
+            cout<<"your character is Rohit Sharma!"<<endl;
+        }
+        else if(e==2){
+            cout<<"does your character bowl?"<<endl;
+            cin>>f;
+            zf++;
+            if(f==1){
+                    cout<<"your character is shardul thakur!"<<endl;
+            }
+            else if(f==2){
+                    cout<<"does your character play for srh in ipl?"<<endl;
+            cin>>g;
+            zf++;
+            if(g==1){
+                cout<<"your character is rahul tripathi"<<endl;
+            }
+            else if(g==2){
+                cout<<"your character is suryakumar yadav"<<endl;
+            }
+        }
+    }
+}
+
+else if(d==2){
+        cout<<"does your character play for chennai super kings in ipl?"<<endl;
+cin>>op;
+zf++;
+if(op==1){
+    cout<<"your character is deepak chahar"<<endl;
+}
+else if(op==2){
+        cout<<"does your character married to a dancer?"<<endl;
+cin>>abc;
+zf++;
+if(abc==1){
+    cout<<"your character is yuzvendra chahal"<<endl;
+}
+else if(abc==2){
+    cout<<"is your character from karnataka?"<<endl;
+    cin>>h;
+    zf++;
+    if(h==1){
+        cout<<"has your character married to a famous celebrity's daughter?!"<<endl;
+        cin>>i;
+        zf++;
+        if(i==1){
+            cout<<"your character is kl rahul"<<endl;
+        }
+        else if(i==2){
+            cout<<"your character is mayank agrawal"<<endl;
+        }
+    }
+    else if(h==2){
+        cout<<"is your character an all rounder?"<<endl;
+        cin>>j;
+        zf++;
+        if(j==1){
+            cout<<"does your character celebrate his fifties/hundreds with sword movement?"<<endl;
+            cin>>k;
+            zf++;
+            if(k==1){
+                cout<<"your character is ravindra jadeja"<<endl;
+            }
+            else if(k==2){
+                cout<<"your character is ravichandran ashwin"<<endl;
+            }
+        }
+        else if(j==2){
+            cout<<"does your character play from delhi in domestic cricket?"<<endl;
+            cin>>l;
+            zf++;
+            if(l==1){
+                cout<<"has your character never ever won ipl trophy?!"<<endl;
+                cin>>i;
+                zf++;
+                if(i==1){
+                    cout<<"your character is virat kohli"<<endl;
+                }
+                else if(i==2){
+                    cout<<"your character is shikhar dhawan"<<endl;
+                }
+            }
+            else if(l==2){
+                cout<<"is your character a pacer?"<<endl;
+                cin>>m;
+                zf++;
+                if(m==1){
+                    cout<<"does your character prefer t20/odi over test cricket?"<<endl;
+                    cin>>n;
+                    zf++;
+                    if(n==1){
+                        cout<<"does your character play for sunrisers hyderbad?"<<endl;
+                        cin>>o;
+                        zf++;
+                        if(o==1){
+                            cout<<"your character is bhuvneshvar kumar"<<endl;
+                        }
+                        else if(o==2){
+                            cout<<"your character is harshal patel"<<endl;
+                        }
+                    }
+                    else if(n==2){
+                        cout<<"has your character took a hat-trick in wc?"<<endl;
+                        cin>>p;
+                        zf++;
+                        if(p==1){
+                            cout<<"your character is mohd shami"<<endl;
+                        }
+                        else if(p==2){
+                            cout<<"has your character played for mumbai indians in past?"<<endl;
+                            cin>>q;
+                            zf++;
+                            if(q==1){
+                                cout<<"your character is jaydev unadkat"<<endl;
+                            }
+                            else if(q==2){
+                                cout<<"your character is umesh yadav"<<endl;
+                            }
+                        }
+                    }
+                }
+                else if(m==2){
+                    cout<<"your character does'nt exist"<<endl;
+                }
+            }
+        }
+    }
+        }
+    }
+}
+}
+}
+if(c==2){
+    cout<<"is your character known for his batting?!"<<endl;
+    cin>>r;
+    zf++;
+    if(r==1){
+        cout<<"does your character open?"<<endl;
+        cin>>s;
+        zf++;
+        if(s==1){
+            cout<<"is your character linked with sara tendulkar?"<<endl;
+            cin>>t;
+            zf++;
+            if(t==1){
+                cout<<"your character is shubman gill"<<endl;
+            }
+            else if(t==2){
+                cout<<"has your character won an orange cap in ipl ever?"<<endl;
+                cin>>u;
+                zf++;
+                if(u==1){
+                    cout<<"your character is ruturaj gaikwad"<<endl;
+                }
+                else if(u==2){
+                    cout<<"does your character's name's meaning is earth?"<<endl;
+                    cin>>hi;
+                    zf++;
+                    if(hi==1){
+                        cout<<"your character is prithvi shaw"<<endl;
+                    }
+                else if(hi==2){
+                        cout<<"your character is ishan kishan"<<endl;
+                    }
+                }
+            }
+        }
+
+        else if(s==2){
+                cout<<"has your character been a captain of a delhi capitals in ipl?"<<endl;
+        cin>>sr;
+        zf++;
+        if(sr==1){
+            cout<<"your character is shreyas iyer"<<endl;
+        }
+        else if(sr==2){
+            cout<<"is your character a keeper-batsmen?"<<endl;
+            cin>>sj;
+
+            if(sj==1){
+                cout<<"your character is sanju samson"<<endl;
+            }
+        else if(sj==2){
+            cout<<"is your character an all rounder?!"<<endl;
+            cin>>v;
+            zf++;
+            if(v==1){
+                cout<<"is your character a medium pacer?"<<endl;
+                cin>>w;
+                zf++;
+                if(w==1){
+                    cout<<"your character is hardik pandya"<<endl;
+                }
+                else if(w==2){
+                    cout<<"is your character in the current squad of test cricket?"<<endl;
+                    cin>>xx;
+                    zf++;
+                    if(xx==1){
+                        cout<<"your character is axar patel"<<endl;
+                    }
+                    else if(xx==2){
+                        cout<<"had your character involved in a fight with krunal pandya in domestic cricket?"<<endl;
+                        cin>>yy;
+                        zf++;
+                        if(yy==1){
+                            cout<<"your character is deepak hooda"<<endl;
+                        }
+                        else if(yy==2){
+                            cout<<"your character is washington sundar"<<endl;
+                        }
+                    }
+                                  }
+            }
+        }
+    }
+        }
+}
+     else if(r==2){
+                cout<<"is your character not ravi bishnoi and under the age of 25"<<endl;
+                cin>>z;
+                zf++;
+                if(z==1){
+                    cout<<"has your character bowled above 155 kph?"<<endl;
+                    cin>>ab;
+                    if(ab==1){
+                        cout<<"your character is umran malik"<<endl;
+                    }
+                    else if(ab==2){
+                        cout<<"your character is arshdeep singh"<<endl;
+                    }
+                }
+                else if(z==2){
+                    cout<<"is your character one of the key bowlers of mumbai indians in ipl?"<<endl;
+                    cin>>cd;
+                    zf++;
+                    if(cd==1){
+                        cout<<"your character is jasprit bumrah"<<endl;
+                    }
+                    else if(cd==2){
+                        cout<<"does your character play for royal challengers banglore in ipl?"<<endl;
+                        cin>>ef;
+                        zf++;
+                        if(ef==1){
+                            cout<<"your character is mohd siraj"<<endl;
+                        }
+                        else if(ef==2){
+                            cout<<"is your character a spinner?"<<endl;
+                            cin>>gh;
+                            zf++;
+                            if(gh==1){
+                                cout<<"has your character took hat-trick in international cricket?"<<endl;
+                                cin>>ij;
+                                zf++;
+                                if(ij==1){
+                                    cout<<"your character is kuldeep yadav"<<endl;
+                                }
+                                else if(ij==2){
+                                    cout<<"your character is ravi bishnoi"<<endl;
+                                }
+                            }
+                            else if(gh==2){
+                                cout<<"your character does'nt exist!"<<endl;
+                            }
+                        }
+                    }
+                }
+            }
+    }
+}
+else if(b==2){
+    cout<<"does your character bowl?"<<endl;
+    cin>>ris;
+    if(ris==1){
+        cout<<"your character is deepti sharma"<<endl;
+    }
+    else if(ris==2){
+        cout<<"your character is smriti mandhana"<<endl;
+    }
+}
+}
+cout<<"did i guess right?!"<<endl;
+cout<<endl;
+cout<<"enter 1 for yes and 2 for no"<<endl;
+cin>>ny;
+if(ny==1){
+    cout<<"thats great! thanks for playing.."<<endl;
+    cout<<endl;
+    lm=1;
+    break;
+}
+else if(ny==2){
+    cout<<"oh sorry, lets try again!"<<endl;
+    lm=0;
+}
+}
+cout<<"i guessed your character in "<<zf<<" questions!"<<endl;
+}
+class rishabh: public jeet
+{
+    public:
+    void namee(){
+    cout<<"enter your name "<<endl;
+    cin>>name;
+    cout<<"your name is = "<<name<<endl;
+    }
+};
+main()
+{
+    rishabh jpp;
+    jpp.namee();
+    jpp.game();
+}
+
+
